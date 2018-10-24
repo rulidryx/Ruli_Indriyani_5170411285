24bd4107824d249b77343701e03405d091dfd65f
rulidryx <44422530+rulidryx@users.noreply.github.com>
2018-10-24 10:22:58 +0700

    Add files via upload

---
+++ PBO/src/Program/Perulangan.java
@@ -0,0 +1,15 @@
+
+package Program;
+import java.util.Scanner;
+public class Perulangan {
+    public static void main(String[] args){
+        int pil;
+        Scanner s = new Scanner (System.in);
+        do{
+        System.out.println("1. Input Data");
+        System.out.println("2. Tampil Data");
+        System.out.println("3. keluar ");
+        System.out.println("Pilih: ");
+        pil=s.nextInt();}
+        while (pil!=3);}
+        }
